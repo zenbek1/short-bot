b5d2c7e1c87d81fdaf8656d429234b9367f99c43
zenbek1 <zenbek.ltd@gmail.com>
2025-07-30 12:15:06 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8461610744:AAGhqcrnJBj9nPuCSHx2oE7ZuMezycJ9Uow"
 
-async def reply_to_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text.lower()
-    if "привет" in text:
-        await update.message.reply_text("Привет! Как дела?")
-    elif "как дела" in text:
-        await update.message.reply_text("Всё отлично! А у тебя?")
+# Команда /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот и работаю успешно 🚀")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    # Добавим хендлер команды /start
+    app.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    app.run_polling()
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply_to_message))
-    app.run_polling()
+    main()
